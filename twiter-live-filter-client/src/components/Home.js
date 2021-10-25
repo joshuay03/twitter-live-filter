2d6080a25f54e5e11d89b82920e18088ccf9cd03
Joshua Young <djry1999@gmail.com>
2021-10-25 23:27:08 +1000

    Initial Home view and api integration

--- twiter-live-filter-client/src/components/Home.js
+++ twiter-live-filter-client/src/components/Home.js
@@ -1,8 +1,103 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
+import needle from 'needle';
 
 export default function Home() {
+  const baseUrl = 'http://localhost:3002/filter'
+  const [queries, setQueries] = useState('');
+  const [apiUrl, setApiUrl] = useState('');
+  const [tweets, setTweets] = useState([]);
+  const textDecoder = new TextDecoder("utf-8");
+
+  function stringToJSON(string) {
+    let parsedData = [];
+
+    if (string.match(/"data":/g).length > 1) {
+      let index1 = string.indexOf('"data":') - 1;
+      let index2 = string.indexOf('"data":', index1 + 8) - 1;
+
+      while (index1 !== string.length) {
+        parsedData.push(JSON.parse(string.slice(index1, index2)).data);
+        index1 = index2;
+        const nextIndex = string.indexOf('"data":', index1 + 8) - 1;
+        index2 = nextIndex > 0 ? nextIndex : string.length;
+      }
+
+      return parsedData;
+    }
+
+    return [JSON.parse(string).data];
+  }
+
+  function updateTweets() {
+    const stream = needle.get(baseUrl);
+    stream
+      .on('data', (data) => {
+        const parsedData = stringToJSON(textDecoder.decode(data));
+        setTweets(tweets => [ ...parsedData, ...tweets ]);
+      })
+      .on('err', (err) => {
+        console.log(err);
+      })
+      .on('done', () => {
+        setQueries('');
+      });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    updateTweets();
+  }
+
+  function handleQueriesChanged(e) {
+    setQueries(e.target.value);
+
+    let queryUrl = '?';
+    let queryCount = 0;
+    const queryList = queries.split(',');
+
+    queryList.forEach((query) => {
+      queryCount++;
+      queryUrl += `query${queryCount}=${query.trim()}&`;
+    });
+    queryUrl = queryUrl.slice(0, queryUrl.length - 1);
+    setApiUrl(baseUrl + queryUrl);
+  }
+
+  useEffect(() => {}, [tweets]);
+
   return (
     <>
+      <div className="bg-blue-400 h-screen block">
+        <div className="h-44 w-full flex justify-center items-end">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Enter comma separated queries (Ex: query1,query2)"
+              value={queries}
+              onChange={(e) => {handleQueriesChanged(e)}}
+              className="h-12 w-search rounded-lg text-xl px-4"
+            />
+            <button
+              type="submit"
+              className="bg-gray-300 w-28 h-12 rounded-lg ml-6 text-xl hover:bg-gray-200"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="grid grid-cols-3 pt-24 pb-12 bg-blue-400">
+          {tweets.map((tweet) => {
+            return (
+              <div
+                 key={tweet.id}
+                 className="h-44 w-card bg-white rounded-2xl m-auto px-4 my-4 grid place-items-center"
+                >
+                {tweet.text}
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- twitter-live-filter-api/routes/filter.js
+++ twitter-live-filter-api/routes/filter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { promisify } = require('util'); 
+const { promisify } = require('util');
 const stream = require('stream');
 const pipeline = promisify(stream.pipeline);
 const got = require('got');
@@ -9,7 +9,7 @@ const filterStream =  new stream.Transform({
     objectMode: true,
     transform(chunk, encoding, callback) {
         // should we pass this chunk onto the next stream in the pipeline
-        let take; 
+        let take;
         try {
             // This is where we'd implement nltk for filtering.
             take = true;
@@ -25,17 +25,17 @@ router.get('/', async (req, res, next) => {
     if (Object.keys(req.query).length > 3) {
         return res.status(400).json({ Error: 'There is a maximum of three tags.' });
     } else {
-        const readStream = got.stream('http://localhost:3001/stream');       
-        
+        const readStream = got.stream('http://localhost:3001/stream');
+
         req.on('end', () => res.end())
             .on('pause', () => readStream.pause())
             .on('resume', () => readStream.resume());
 
         readStream.on('error', (error) => console.log(error));
 
-        pipeline(readStream, filterStream, res)
-            .catch((error) => console.log(error));        
-    }    
+        pipeline(readStream, res)
+            .catch((error) => console.log(error));
+    }
 });
 
 module.exports = router;
